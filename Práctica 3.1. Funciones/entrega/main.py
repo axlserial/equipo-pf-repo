5a39a021ca95b5bc08a445c789456cd245c12730
Maggy30ev <maggy30ev@gmail.com>
2022-12-13 01:11:48 -0600

    Correción

--- Práctica 3.1. Funciones/entrega/main.py
+++ Práctica 3.1. Funciones/entrega/main.py
@@ -1,22 +1,5 @@
-import csv
-
-f = './vgsales.csv'
-
-
-def all_registers(name_file: str)-> tuple[tuple[str,...],...]:
-    '''Devuelve una tupla de tuplas con todos los registros y todas las columnas del archivo vgsales.csv'''
-    
-    with open(name_file) as csvfile:
-        games = csv.reader(csvfile, delimiter=',')
-        h = next(games)
-
-        tuple_games = tuple(tuple(game) for game in games)
-
-        return tuple_games
-
-
 def main():
-    datos = all_registers(f)
+    pass
     
 
 if __name__ == '__main__':
---
+++ Práctica 3.1. Funciones/entrega/utils.py
@@ -0,0 +1,15 @@
+import csv
+
+f = './vgsales.csv'
+
+
+def all_registers(name_file: str)-> tuple[tuple[str,...],...]:
+    '''Devuelve una tupla de tuplas con todos los registros y todas las columnas del archivo vgsales.csv'''
+    
+    with open(name_file) as csvfile:
+        games = csv.reader(csvfile, delimiter=',')
+        h = next(games)
+
+        tuple_games = tuple(tuple(game) for game in games)
+
+        return tuple_games
