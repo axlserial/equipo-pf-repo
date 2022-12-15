ef74b6eb458be8072126caea3aa128f6cc08f50c
Maggy30ev <maggy30ev@gmail.com>
2022-12-15 02:13:52 -0600

    set_comp y  cambios en: utils, main y dict

--- Práctica 3.1. Funciones/entrega/main.py
+++ Práctica 3.1. Funciones/entrega/main.py
@@ -1,13 +1,21 @@
 from utils import all_registers
 from pprint import pprint
 import dict_comp as dc
+import set_comp as sc
 
 
 def main():
     h, data = all_registers('./vgsales.csv')
 
+
     # Diccionary Comprehensions
+    print("-------------------- Diccionary Comprehensions --------------------\n")
     print_dicts(h, data)
+    print("\n-----------------------------------------------------------------")
+    # Set Comprehensions
+    print("\n-------------------- Set Comprehensions --------------------\n")
+    print_sets(h, data)
+    print("\n-----------------------------------------------------------------")
 
 
 # ------------------ Dict Comps ----------------
@@ -45,6 +53,43 @@ def print_dicts(h: tuple[str, ...], data: tuple[tuple[str, ...], ...]) -> None:
 
 
 # ----------------------------------------------
+
+# ------------------ Set Comps ----------------
+
+def print_sets(h: tuple[str, ...], data: tuple[tuple[str, ...], ...]) -> None:
+    print(
+        "1. Conjunto de los nombres de las plataformas."
+    )
+    set_one = sc.first_set_comp(data, h)
+    pprint(set(list(set_one)[:5]))
+
+    print(
+        "\n2. Conjunto de todos los registros que sus elementos sean la tupla "
+        "'Name' y 'Global_Sales' de los juegos."
+    )
+    set_two = sc.second_set_comp(data, h)
+    pprint(set(list(set_two)[:5]))
+
+    print(
+        "\n3. Conjunto de todos los registros donde sus elementos sean la tupla "
+        "'Name' y 'NA_Sales' de los juegos que hayan vendido más de 5.0 "
+        "millones en NA")
+    set_three = sc.third_set_comp(data, h)
+    pprint(set(list(set_three)[:5]))
+
+    print(
+        "\n4. Conjunto de tuplas con las columnas: 'Name' y 'Global_Sales' de juegos "
+        "donde su 'Publisher' sea Sega, su 'Platform' sea 3DS y que contengan la palabra Sonic"
+    )
+    set_four = sc.fourth_set_comp(data, h)
+    pprint(set(list(set_four)[:5]))
+
+    print(
+        "\n5. Conjunto de los nombres de los 'Publisher' que poseen juegos del "
+        "genero 'Platform' cuyo lanzamiento esté entre los años 2013 y "
+        "2016")
+    set_five = sc.fifth_set_comp(data, h)
+    pprint(set(list(set_five)[:5]))
 
 if __name__ == '__main__':
     main()
--- Práctica 3.1. Funciones/entrega/utils.py
+++ Práctica 3.1. Funciones/entrega/utils.py
@@ -1,4 +1,6 @@
 import csv
+from typing import Callable
+
 
 f = './vgsales.csv'
 
@@ -15,3 +17,21 @@ def all_registers(
         tuple_games = tuple(tuple(game) for game in games)
 
         return (h, tuple_games)
+
+
+# Closure para seleccionar una columna de una tupla.
+def select_column(column: str, h: tuple[str, ...]) -> Callable:
+
+    def inner(data: tuple):
+        return data[h.index(column)]
+
+    return inner
+
+
+# Closure para filtrar una tupla de tuplas por una columna y un valor.
+def filter_column(column: str, value, h: tuple[str, ...]) -> Callable:
+
+    def inner(data: tuple) -> tuple:
+        return tuple([d for d in data if d[h.index(column)] == value])
+
+    return inner
