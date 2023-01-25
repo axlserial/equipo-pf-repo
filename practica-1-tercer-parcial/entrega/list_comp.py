521df5973889a91c8ed67f504eadcb6e0cc2868e
kevyx-gif <apolortms@gmail.com>
2023-01-24 20:16:25 -0600

    Full Listas

--- practica-1-tercer-parcial/entrega/list_comp.py
+++ practica-1-tercer-parcial/entrega/list_comp.py
@@ -1,14 +1,6 @@
 from typing import Callable
 import csv
 
-def promedio(precios: str) -> float:
-    prom: float = 0.0
-    for i in precios:
-        prom = prom + float(i[1])
-
-    return round((prom / 5), 2)
-
-
 def quitarElementos(diccionario):
     return {'Rank':diccionario['Rank'], 'Name':diccionario['Name'],'Platform':diccionario['Platform'],'Genre':diccionario['Genre'],'Year':diccionario['Year']}
 
@@ -26,10 +18,34 @@ def actividades(csvName) -> Callable:
     yield lista
 
 
-    #----------Primera actividad, de la lista extraida solo dejar el diccionario con nombre, rango, plataforma, añoa, género---------#
+    #----------Actividad, de la lista extraida(del csv) solo dejar el diccionario con nombre, rango, plataforma, año, género---------#
 
-    listaFiltrada = map(quitarElementos,lista)
+    print("-----------Actividad 1, quitar informacion--------")
+    yield map(quitarElementos,lista)
 
-    print("-----------Lista Filtrada--------")
-    yield list(listaFiltrada)
+    #----------Actividad, de la lista extraida(del csv) solo dejar los juegos de plataforma Wii
 
+    print("-----------Actividad 2, dejar solo plataforma Wii--------")
+
+    yield filter(lambda x : x['Platform'] == 'Wii',lista)
+
+    #----------Actividad, de la lista extraida(del csv) filtrar por el año 2006 y solo mostrar el rango, el nombre y su género
+
+    print("-----------Actividad 3, Filtrar por año 2006 y mostrar nombre,rango,genero--------")
+
+    yield map(lambda x : {'Rank':x['Rank'],'Name':x['Name'],'Genre':x['Genre']},filter(lambda x : x['Year'] == '2006',lista))
+
+    #----------Actividad, de la lista extraida(del csv) usar Generator comprehensions para tomar todos los juegos publicados por nintendo y sacar el promedio de Sales
+
+    print("-----------Actividad 4, tomar juegos publicados por nintendo y sacar promedio de sus Sales")
+    
+    def sumaT(x):
+        return float(x['NA_Sales']) + float(x['EU_Sales']) + float(x['JP_Sales']) + float(x['Other_Sales']) + float(x['Global_Sales'])
+
+    yield ({'Name':i['Name'], 'Publisher':i['Publisher'] , 'Platform':['Platform'] , 'Sales' : round(sumaT(i)/5,2) } for i in lista if i['Publisher'] == 'Nintendo')
+
+    #----------Actividad, de la lista extraida(del csv) con un generador ordenar los juegos por año de salida pero que se presenten del mas actual al mas viejo (filtrar los que no tengan año N/A ), que se muestre año, nombre y rango.
+
+    print("-----------Actividad 5, Ordenar por año del mas nuevo al mas viejo mostrando  año, nombre y rango")
+
+    yield ( {'Year':i['Year'] , 'Name':i['Name'] , 'Rank':i['Rank']} for i in reversed(sorted( filter( lambda x : x['Year'] != 'N/A' and x['Year'] != 'Adventure',lista), key = lambda x : x['Year'])) )
--- practica-1-tercer-parcial/entrega/main.py
+++ practica-1-tercer-parcial/entrega/main.py
@@ -4,9 +4,9 @@ import list_comp as lc
 
 def main():
 
-    # List Comprehensions
+    # List 
     print(
-        "-------------------- List Comprehensions --------------------------\n"
+        "-------------------- List  --------------------------\n"
     )
     print_list()
     print(
@@ -20,8 +20,22 @@ def main():
 
 def print_list() -> None:
     dir_csv = "./practica-1-tercer-parcial/entrega/vgsales.csv"
+    
+    #Ciclo que sirve como iteradores encadenados y nos muestra las 5 actividades, genera una lista del CSV y musestra las 5 actividades puestas en el modulo List_comp.py
     for i in lc.actividades(dir_csv):
-        print(i[:3])
+
+        #Comprueba que sea un iterador (Object)
+        if (isinstance(i,object) ):
+            for j,k in enumerate(i): 
+                #Usando enumerate vamos a verificar que solo 3 se impriman, esto para no imprimir muchos juegos y puedan diferenciarse las actividades
+                if j == 3:
+                    print(k)
+                    break
+                print(k)
+
+        # si no es un iterador es un iterable o lista
+        else:
+            print(i[:4])
 
 if __name__ == '__main__':
     main()
